69539c899dd2073730c4d4e2f3f7378cc9045d6a
Agus Eko Setiyono <agusesetiyono@gmail.com>
2014-04-15 13:06:09 +0700

    fix featured

---
+++ _site/flt/js/latihan.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+	$('button').attr('data-effect', 'st-effect-3').click(function(event) {
+		$('.st-container').toggleClass('st-effect-3 st-menu-open');
+	});;
+
+
+});
+
+
+
+// 100% Height
+//====================================================== 
+// $("#porto").css("height", $(window).height() + "px");
+$("#intro").css("height", $(window).height() + "px");
