69061254212eb0e5227fb1495750b74017e03795
agusesetiyono <agusesetiyono@gmail.com>
2015-07-09 09:23:57 +0700

    edit json

--- jne/js/fuse.js
+++ jne/js/fuse.js
@@ -56,7 +56,7 @@ $(function() {
     createFuse();
   }
 
-  $.getJSON('../json/jakarta.json', function(data) {
+  $.getJSON('http://agusesetiyono.16mb.com/jne/json/jakarta.json', function(data) {
      start(data);
   });
 
